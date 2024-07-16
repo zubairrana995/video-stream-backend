9162f485fe1398165f21f158f91b51c2cb075979
zubair rana <zubairrana995@gmail.com>
2024-07-16 18:31:12 +0500

    create first route

--- src/app.js
+++ src/app.js
@@ -16,4 +16,10 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+//route import
+import userRouter from "./routes/user.routes.js";
+
+//routers declaration
+app.use("/api/v1/users", userRouter);
+
 export { app };
